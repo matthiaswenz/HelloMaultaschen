70a84a8df4250a63299ae671ee98a4691ce4759f
Matthias Wenz <matwen@microsoft.com>
2017-02-06 16:16:54 +0100

    Add smoke launch test

---
+++ app/src/androidTest/java/com/microsoft/hellomaultaschen/LaunchAppTest.java
@@ -0,0 +1,30 @@
+package com.microsoft.hellomaultaschen;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LaunchAppTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void testLaunchActivity() {
+        onView(withId(R.id.textView)).check(matches(withText("Hello Maultaschen!")));
+    }
+
+
+}
