f78c955c10342e34dc97eada552982df4a615d81
Matthias Wenz <matwen@microsoft.com>
2017-02-06 16:19:02 +0100

    Add XTC Espresso integration

--- app/src/androidTest/java/com/microsoft/hellomaultaschen/LaunchAppTest.java
+++ app/src/androidTest/java/com/microsoft/hellomaultaschen/LaunchAppTest.java
@@ -4,6 +4,10 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
+
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,9 +25,17 @@ public class LaunchAppTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void testLaunchActivity() {
         onView(withId(R.id.textView)).check(matches(withText("Hello Maultaschen!")));
+    }
+
+    @After
+    public void tearDown() {
+        reportHelper.label("Stopping app");
     }
 
 
